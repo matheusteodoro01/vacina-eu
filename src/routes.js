2df2fbcab7ca2e9f50da3051fd377a42bb1bf758
Matheus Teodoro <matheusteodoro01@hotmail.com>
2021-06-15 01:49:01 -0300

    remove

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register'
 import Agendar from './pages/Agendar'
-import Scheduling from './pages/Scheduling'
 import Profile from './pages/Profile'
 import NewIncident from './pages/NewIncident'
 export default function Routes() {
@@ -14,7 +13,6 @@ export default function Routes() {
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register}/>
                 <Route path="/agendar" component={Agendar}/>
-                <Route path="/scheduling" component={Scheduling}/>
                 <Route path="/profile" component={Profile}/>
                 <Route path="/incidents/new" component={NewIncident}/>
             </Switch>
